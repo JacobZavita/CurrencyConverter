fa3408dd51e4245ce0dff8b8ffdc3f894ad45a42
Jeckfeldt <jeckfeldt123@gmail.com>
2021-05-25 14:19:00 -0700

    added fiat to fiat conversion

--- calculator.js
+++ calculator.js
@@ -14,18 +14,31 @@ const fiatToFiat = () => {
   //get fiat results(includes exchange rates)
   axios.get(`http://api.currencylayer.com/live?access_key=34eca9d22b34a8f77ebe7de351ba880e&format=1&source=${document.getElementById('from').value}`)
     .then(res => {
-      let source = res.data.source
-      console.log(source)
-      let quotes = res.data.quotes
-      console.log(quotes)
+      //get the conversions dictionary
+      let conversions = res.data.quotes
+
+      //get key string of the "from" str and the "to" str
+      str = document.getElementById('from').value + document.getElementById('to').value
+      
+      //multiply the amount by conversion rate
+      let result = parseFloat(document.getElementById('amount').value) * conversions[str.toUpperCase()]
+      printResult(result)
+
     })
     .catch(err => console.error(err))
   }
+
+
+//function to print out the answer
+//expects a number value !!!!!!!!!!!!!!!!!!Still in progress
+const printResult = (result) => {
+  console.log(result)
+}
+
 //start logic on conversion
 
 document.getElementById('convertCalc').addEventListener('click', event => {
-  //do 4 different functions depending on the checkboxes
-
+  //determine which checkboxes are checked
   if(document.getElementById('fromFiat').checked) {
     fiatToFiat()
   }
