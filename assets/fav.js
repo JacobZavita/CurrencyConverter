b99cf01efdfd28a0973aefc0a44e8443aebe12f3
Charlotte Wong <ChiharuTaru@gmail.com>
2021-05-26 20:33:32 -0700

    implemented functions to render fiat and crypto rows

--- assets/fav.js
+++ assets/fav.js
@@ -5,6 +5,89 @@
 
 // add option to delete items from favs with "X" button
 
+fiatArray = [
+  {
+    code: 'USD',
+    name: 'United States Dollar'
+  },
+  {
+    code: 'CNY',
+    name: 'Chinese Yuan'
+  },
+  {
+    code: 'JPY',
+    name: 'Japanese Yen'
+  },
+  {
+    code: 'EUR',
+    name: 'Euro'
+  },
+  {
+    code: 'GBP',
+    name: 'British Pound Sterling'
+  },
+  {
+    code: 'INR',
+    name: 'Indian Rupee'
+  },
+  {
+    code: 'AUD',
+    name: 'Australian Dollar'
+  },
+  {
+    code: 'CAD',
+    name: 'Canadian Dollar'
+  },
+  {
+    code: 'CHF',
+    name: 'Swiss Franc'
+  },
+  {
+    code: 'RUB',
+    name: 'Russian Ruble'
+  },
+  {
+    code: 'HKD',
+    name: 'Hong Kong Dollar'
+  },
+  {
+    code: 'NZD',
+    name: 'New Zealand Dollar'
+  },
+  {
+    code: 'BRL',
+    name: 'Brazilian Real'
+  },
+  {
+    code: 'NGN',
+    name: 'Nigerian Naira'
+  },
+  {
+    code: 'KRW',
+    name: 'Korean Won'
+  },
+  {
+    code: 'IDR',
+    name: 'Indonesean Rupah'
+  },
+  {
+    code: 'SAR',
+    name: 'Saudi Riyal'
+  },
+  {
+    code: 'TRY',
+    name: 'Turkish'
+  },
+  {
+    code: 'KWD',
+    name: 'Kuwait Dinar'
+  },
+  {
+    code: 'KYD',
+    name: 'Cayman Island Dollar'
+  }
+]
+
 // dayAgo and weekAgo f(x) I wrote before
 const weekAgo = _ => {
   let formatedDate = ''
@@ -46,19 +129,74 @@ const dayAgo = _ => {
 
   return formatedDate
 }
-//give this guy the data from the axios, res
-const renderCrypto = (cryDat) =>{
 
+//returns the change of fiat over a week or day
+const fiatChange = (past, today) => {
+  return Number((past / today) - 1).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 3 })
 }
+
+//give this guy the data from the axios, res
+const renderCrypto = (cryDat) => {
+  //render crypto
+  let favRow = documet.createElement('tr')
+  favRow.classList.add('testRow')
+  favRow.innerHTML = `
+    <td>${cryDat.symbol}</td>
+    <td>$${cryDat.price}</td>
+    <td>${cryDat.percent_change_24h}%</td>
+    <td>${cryDat.percent_change_7d}%</td>
+    <td><a class="rm-btn waves-effect waves-light btn red">X</a></td>
+  `
+  // append
+  document.getElementById('favData').append(favRow)
+}
+
+// render fiat data
+const renderFiat = (fiDat) => {
+  const baseCurrency = 'USD'
+  axios.get(`http://api.currencylayer.com/live?access_key=34eca9d22b34a8f77ebe7de351ba880e&source=${baseCurrency}&format=1`)
+    .then(res => {
+      const fiat = res.data.quotes // same as quotes in app.js
+      // one week ago data
+      axios.get(`https://api.currencylayer.com/historical?access_key=34eca9d22b34a8f77ebe7de351ba880e&date=${weekAgo()}`)
+        .then(resp => {
+          const fiatWeekAgo = resp.data.quotes
+          // one day ago data
+          axios.get(`https://api.currencylayer.com/historical?access_key=34eca9d22b34a8f77ebe7de351ba880e&date=${dayAgo()}`)
+            .then(respo => {
+              const fiatDayAgo = respo.data.quotes
+              // render fiat here
+              let favRow = document.createElement('tr')
+              favRow.classList.add('testRow')
+              const exCode = 'USD' + fiDat
+              // calculate day ago rate and week ago rate
+              const dayPercent = fiatChange(fiatDayAgo[exCode], fiat[exCode])
+              const weekPercent = fiatChange(fiatWeekAgo[exCode], fiat[exCode])
+              favRow.innerHTML = `
+                <td>${fiDat}</td>
+                <td>$${fiat[exCode]}</td>
+                <td>${dayPercent}%</td>
+                <td>${weekPercent}%</td>
+                <td><a class="rm-btn waves-effect waves-light btn red">X</a></td>
+              `
+              // append
+              document.getElementById('favData').append(favRow)
+            })
+            .catch(err => console.error(err))
+        })
+        .catch(err => console.error(err))
+    })
+    .catch(err => console.error(err))
+}
+
 // give this guy the uh data from the local storage array,
 // and object with attributes code(string) and fiat(string, contains 'true' or 'false')
 // maybe nest it all?
-
-
-
+// you can index the array at [i] to get "codeData"
 const renderItem = (codeData) => {
   if (codeData.fiat === 'true') {
     // fiat request here
+    renderFiat(codeData.code)
   } else if (codeData.fiat === 'false') {
     // crypto request here
     axios.get(`https://api.lunarcrush.com/v2?data=assets&key=nt1go9e8ol889xc2mjy0g&symbol=${codeData.code}`)
@@ -68,22 +206,10 @@ const renderItem = (codeData) => {
         renderCrypto(cryDat)
       })
       .catch(err => console.error(err))
-  }
-  else {
+  } else {
     console.error('Error with codeData.fiat in renderItem f(x) in fav.js')
   }
 
+// render all the data upon landing on fav page
 
-
-  // create elem stuff
-  const fav = document.createElement('tr')
-  fav.classList.add('testRow')
-  fav.innerHTML = `
-    <td>Euro</td>
-    <td>$1.22</td>
-    <td>-0.014%</td>
-    <td>0.14%</td>
-    <td><a class="waves-effect waves-light btn red">X</a></td>
-  `
-  document.getElementById('favData').append(fav)
 }
