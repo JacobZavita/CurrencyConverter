7e829e0fa5ef3d6e317c54d5e37d7de0788f9f37
Charlotte Wong <ChiharuTaru@gmail.com>
2021-05-26 19:39:41 -0700

    added functions to fav.js

--- assets/fav.js
+++ assets/fav.js
@@ -5,19 +5,77 @@
 
 // add option to delete items from favs with "X" button
 
+// dayAgo and weekAgo f(x) I wrote before
+const weekAgo = _ => {
+  let formatedDate = ''
+  // Get today's date using the JavaScript Date object.
+  let ourDate = new Date()
+  // Change it so that it is 7 days in the past.
+  let pastDate = ourDate.getDate() - 7
+  ourDate.setDate(pastDate)
+  ourDate += ''
+  // get year
+  formatedDate += ourDate.substring(11, 15)
+  formatedDate += '-'
+  // get month
+  formatedDate += monthToNumber(ourDate.substring(4, 7))
+  formatedDate += '-'
+  // get day
+  formatedDate += ourDate.substring(8, 10)
+
+  return formatedDate
+}
+
+// returns formated date of one day ago
+const dayAgo = _ => {
+  let formatedDate = ''
+  // Get today's date using the JavaScript Date object.
+  let ourDate = new Date()
+  // Change it so that it is 1 day in the past.
+  let pastDate = ourDate.getDate() - 1
+  ourDate.setDate(pastDate)
+  ourDate += ''
+  // get year
+  formatedDate += ourDate.substring(11, 15)
+  formatedDate += '-'
+  // get month
+  formatedDate += monthToNumber(ourDate.substring(4, 7))
+  formatedDate += '-'
+  // get day
+  formatedDate += ourDate.substring(8, 10)
+
+  return formatedDate
+}
+//give this guy the data from the axios, res
+const renderCrypto = (cryDat) =>{
+
+}
 // give this guy the uh data from the local storage array,
 // and object with attributes code(string) and fiat(string, contains 'true' or 'false')
-const renderItem = (codeData) => {
+// maybe nest it all?
 
+
+
+const renderItem = (codeData) => {
   if (codeData.fiat === 'true') {
     // fiat request here
   } else if (codeData.fiat === 'false') {
     // crypto request here
+    axios.get(`https://api.lunarcrush.com/v2?data=assets&key=nt1go9e8ol889xc2mjy0g&symbol=${codeData.code}`)
+      .then(respons => {
+        //render the crypto data here,
+        let cryDat = respons.data.data
+        renderCrypto(cryDat)
+      })
+      .catch(err => console.error(err))
   }
   else {
     console.error('Error with codeData.fiat in renderItem f(x) in fav.js')
   }
 
+
+
+  // create elem stuff
   const fav = document.createElement('tr')
   fav.classList.add('testRow')
   fav.innerHTML = `
